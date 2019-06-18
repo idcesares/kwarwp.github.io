9508a22d59d0990e8b8c75c9100fc50f664eb82f
carlo <carlo@nce.ufrj.br>
2019-06-18 11:34:31 -0300

    Working parser for simple markdown cell

--- kwarwp/code/kwarwp/browser.py
+++ kwarwp/code/kwarwp/browser.py
@@ -4,8 +4,18 @@ class MMock(MagicMock):
     def __le__(self, _):
         return MagicMock()
 
-class dom:
+class dom(dict):
+    head = MMock()
     pass
+    def __le__(self, _):
+        return MagicMock()
+
+
+class html:
+    DIV = MMock()
+    def __init__(self, *_, **__):
+        self.value = ""
+        pass
     def __le__(self, _):
         return MagicMock()
 '''
@@ -21,9 +31,11 @@ class timer:
 class ajax:
     pass
 '''
-window = win = timer = ajax = html = MMock()
+window = win = timer = ajax = MMock()
 html.__le__ = MagicMock()
 window = win
-document = {"pydiv": dom()}
-document.head = MMock()
+document = dom(pydiv= dom())
+document.head = dom()
+html.DIV = html.STYLE = html.IMG = html.bind = html.H1 = html.H2 = html.A = html.CODE = html.PRE = html
+html.style = MMock()
 
